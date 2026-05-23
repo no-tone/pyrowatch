be21fda92f8993921ea11c245b7a4ddab49ebda8
n0-tone <155180573+no-tone@users.noreply.github.com>
2026-05-23 18:14:09 +0100

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gh.no-tone.com',
-  base: '/pyrowatch',
+  site: 'https://gh.no-tone.com/pyrowatch'
 });
